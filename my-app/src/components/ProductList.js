2c4d96e0068e2060ef7cc07dfeeddf8535c9c307
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-01-30 21:18:01 +0100

    Create ProductList.js

---
+++ my-app/src/components/ProductList.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const products = [
+  { id: 1, name: "Product 1", description: "This is product 1" },
+  { id: 2, name: "Product 2", description: "This is product 2" },
+  { id: 3, name: "Product 3", description: "This is product 3" },
+];
+
+function ProductsList() {
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <div>
+      <h1>Products List</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - {product.description}
+            <button onClick={() => handleAddToCart(product)}>
+              Add to Cart
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductsList;
