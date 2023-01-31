ff8381a245ba5cac30f7517d37359f6bef86635b
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2023-01-31 14:42:13 +0100

    changed items display methode and added delete action

--- my-app/src/components/CartPage.js
+++ my-app/src/components/CartPage.js
@@ -8,23 +8,45 @@ const CartPage = () => {
     // fetch cart items from local storage or API
     const items = JSON.parse(localStorage.getItem("cart")) || [];
     setCartItems(items);
-    console.log("fasf");
   }, []);
+
+  const handleDelete = (id) => {
+    const newCart = cartItems.filter((item) => item.id !== id);
+    setCartItems(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+  };
 
   return (
     <div>
       <h2>Cart Page</h2>
-      <ul>
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            {item.id}
-            <br></br>
-            {item.name}
-            <br></br>
-            {item.description}
-          </li>
-        ))}
-      </ul>
+      {cartItems.length > 0 ? (
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button onClick={() => handleDelete(item.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No items in cart</p>
+      )}
       <Link to="/">Back</Link>
     </div>
   );
